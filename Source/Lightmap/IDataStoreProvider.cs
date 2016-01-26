606e8e64dd767098461524cc67f080df5d1001a5
Johnathon Sullinger <Johnathon@sullinger.us>
2016-01-25 22:31:35 -0700

    Added initial IDataStoreProvider interface.

---
+++ Source/Lightmap/IDataStoreProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lightmap
+{
+    public interface IDataStoreProvider
+    {
+        Task OpenConnectionAsync();
+
+        void OpenConnection();
+    }
+}
