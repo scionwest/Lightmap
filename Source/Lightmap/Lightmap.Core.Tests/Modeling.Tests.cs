6762f648c6edbd676a8320716148699720e38152
Johnathon Sullinger <Johnathon@sullinger.us>
2016-01-31 01:55:17 -0700

    Cleaned up unit test.

--- Source/Lightmap/Lightmap.Core.Tests/Modeling.Tests.cs
+++ Source/Lightmap/Lightmap.Core.Tests/Modeling.Tests.cs
@@ -20,37 +20,17 @@ namespace Lightmap.Provider.Sqlite.Tests
         [Fact]
         public void DatabaseModeler_provides_table_modeler()
         {
-            // Arrange
-            int maxCount = 100000;
-            var elapsed = new List<double>();
-
             // Act
             var modeler = new DatabaseModeler();
 
-            for (int count = 0; count < maxCount; count++)
-            {
-                var watch = new Stopwatch();
-                watch.Start();
-                modeler.Create()
-                    .Table("User", (c) => new
-                    {
-                        UserId = c.NewColumn<int>().AsPrimaryKey(),
-                        FirstName = c.NewColumn<string>().NotNull(),
-                        LastName = c.NewColumn<string>().NotNull(),
-                        MiddleInitial = c.NewColumn<string>()
-                    });
-
-                //.Table(
-                //    "Account",
-                //    () => new { AccountId = default(int), Number = default(double), UserId = default(int) });
-                watch.Stop();
-                elapsed.Add(watch.Elapsed.TotalMilliseconds);
-            }
-
-            double average = elapsed.Average();
-            Debug.WriteLine($"Average is {average}");
+            modeler.Create()
+                .Table("User", (c) => new
+                {
+                    UserId = c.NewColumn<int>().AsPrimaryKey(),
+                    FirstName = c.NewColumn<string>().NotNull(),
+                    LastName = c.NewColumn<string>().NotNull(),
+                    MiddleInitial = c.NewColumn<string>()
+                });
         }
-
-
     }
 }
