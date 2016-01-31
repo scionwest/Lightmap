fca74c0a0fc4a27cbf5e889be5180cea49a14eb1
Johnathon Sullinger <Johnathon@sullinger.us>
2016-01-31 02:09:30 -0700

    Removed test code from the unit test.

--- Source/Lightmap/Lightmap.Core.Tests/Modeling.Tests.cs
+++ Source/Lightmap/Lightmap.Core.Tests/Modeling.Tests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Net;
-using Lightmap.Modeling;
+﻿using Lightmap.Modeling;
 using Xunit;
 
 namespace Lightmap.Provider.Sqlite.Tests
@@ -12,11 +7,6 @@ namespace Lightmap.Provider.Sqlite.Tests
     // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
     public class DatabaseManagerSqliteExtensionsTests
     {
-        public void test(IPAddress ip, int port)
-        {
-
-        }
-
         [Fact]
         public void DatabaseModeler_provides_table_modeler()
         {
